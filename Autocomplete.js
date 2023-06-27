92ff90560d404963089e21a08ee2b57d9a96c467
bhuwan-crossml <132264068+bhuwan-CrossMl@users.noreply.github.com>
2023-06-27 12:44:56 +0530

    Autocomplete.js

--- Autocomplete.js
+++ Autocomplete.js
@@ -1,1 +1,38 @@
 
+  <Autocomplete
+                        sx={{ width: "50%" }}
+                        getItemValue={(e) => e.name}
+                        options={assignedTo}
+                        renderInput={(params) => (
+                          <TextField
+                            error={
+                              !!(
+                                formik.touched.assigned_to &&
+                                formik.errors.assigned_to
+                              )
+                            }
+                            helperText={
+                              formik.touched.assigned_to &&
+                              formik.errors.assigned_to
+                            }
+                            label="Assigned To"
+                            name="assigned_to"
+                            onBlur={formik.handleBlur}
+                            type="text" // ---- WE will not Give , Value & Handle Change to TextFiled ------------>
+                            {...params}
+                          />
+                        )}
+                        getOptionLabel={(option) => option.name} //------ this is Used To Show - Option inside AutoComplete ----------->
+                        // value={undefined} //  ------------ Initial Value of Autocomplet ------------------>
+                        value={formik.values.assigned_to !== "" ? formik.values.assigned_to: "" || ""}
+                        onChange={(e, value) => {
+                             //--- Assining Entire Obj Here, at the time of Sending PayLoad , we will send ,Value.name -------->
+                          formik.setFieldValue("assigned_to", value); // --- we can Assign, Value.name Here , But if we Get an err then we can pass entire Value ------>
+                          //  <-------------------------- . used To acess data from Obj -------------------------------->
+                        }}
+                        // onChange={(e, value) => {                 <----------------- Map is used To Traverse Over - Array ----------------->
+                        //   const names = value.map((item) => ({ name: item.name }));
+                        //   formik.setFieldValue("assigned_to", names);
+                        // }}
+                        // onSelect={handleSelect}
+                      />
