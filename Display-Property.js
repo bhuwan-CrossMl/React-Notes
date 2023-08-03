bdf6893a6a2e843b25bab9927e369f7736d49572
bhuwan-crossml <132264068+bhuwan-CrossMl@users.noreply.github.com>
2023-08-03 17:51:49 +0530

    Create Display-Property.js

---
+++ Display-Property.js
@@ -0,0 +1,4 @@
+display:"flex",
+  flexDirection:'column',
+  alignItems:"center",   // this wiil Center from- Left-Right
+  justifyContent:'center'   // this wiil Center from- Top-Bottom/ center of page
